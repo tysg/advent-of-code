47d1dff437fec9d8d1925d9062b368ccc010f6a0
Tianyi Song <42670338+tysng@users.noreply.github.com>
2020-12-03 23:52:33 +0800

    Complete day 1

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,43 @@ fn main() {
         Some(result) => println!("{}", result.0 * result.1),
         None => println!("no result"),
     }
+
+    match three_sum(&numbers, 2020) {
+        Some(result) => println!("{}", result.0 * result.1 * result.2),
+        None => println!("no result"),
+    }
+
+
+
 }
 
-fn find_pair_sums_to(arr: &Vec<u32>, target: u32) -> Option<(u32, u32)> {
+fn find_pair_sums_to(arr: &[u32], target: u32) -> Option<(u32, u32)> {
     let mut hash: HashMap<u32, usize> = HashMap::new();
 
     for (i, num) in arr.iter().enumerate() {
+        if num > &target {
+            continue;
+        }
         match hash.get(num) {
-            Some(&index) => return Some((num.clone(), arr[index])),
+            Some(&index) => return Some((*num, arr[index])),
             None => hash.insert(target - num, i),
         };
     }
     None
 }
+
+
+
+fn three_sum(arr: &[u32], target: u32) -> Option<(u32, u32, u32)> {
+    for (i, num) in arr.iter().enumerate() {
+        let res = find_pair_sums_to(&arr[i..], target - num);
+        match res {
+            Some((a,b)) => return Some((*num, a, b)),
+            None => continue
+        };
+    }
+    None
+}
+
+
+
