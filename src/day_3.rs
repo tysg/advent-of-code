f22661f23698f044682a2a3ac33264c56d7ee052
Tianyi Song <42670338+tysng@users.noreply.github.com>
2020-12-05 17:03:19 +0800

    Complete day 3

---
+++ src/day_3.rs
@@ -0,0 +1,62 @@
+use std::fs;
+
+fn next(
+    (x, y): (usize, usize),
+    dx: usize,
+    dy: usize,
+    width: usize,
+    height: usize,
+) -> Option<(usize, usize)> {
+    let ny = y + dy;
+    if ny >= height {
+        return None;
+    }
+
+    Some(((x + dx) % width, ny))
+}
+
+#[derive(Debug)]
+struct Game<'a> {
+    map: &'a Vec<Vec<u8>>,
+    width: usize,
+    height: usize,
+}
+
+pub fn solve(filename: &str) {
+    let map = fs::read_to_string(filename)
+        .expect("Something wrong reading file")
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| l.bytes().collect::<Vec<u8>>())
+        .collect::<Vec<Vec<u8>>>();
+
+    let game = Game {
+        width: map[0].len(),
+        height: map.len(),
+        map: &map,
+    };
+
+    let policies = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let product: u64= policies
+        .iter()
+        .map(|(dx, dy)| count_policy_hits(*dx, *dy, &game))
+        .filter(|n| n != &0)
+        .product();
+
+    println!("{}", product);
+}
+
+fn count_policy_hits(dx: usize, dy: usize, game: &Game) -> u64{
+    let mut pos = (0, 0);
+    let mut count = 0;
+
+    while let Some(npos) = next(pos, dx, dy, game.width, game.height) {
+        let (nx, ny) = npos;
+        if game.map[ny][nx] == '#' as u8 {
+            count += 1;
+        }
+        pos = npos;
+    }
+
+    count
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 mod day_1;
 mod day_2;
+mod day_3;
 mod util;
 
 fn main() {
@@ -9,6 +10,7 @@ fn main() {
     match day_number {
         1 => day_1::solve(&args[2]),
         2 => day_2::solve(&args[2]),
+        3 => day_3::solve(&args[2]),
         _ => println!("wrong day_number!"),
     }
 }
