e662b81df2153ba35e00e28940bb5d11c47b4104
Tianyi Song <42670338+tysng@users.noreply.github.com>
2020-12-08 18:26:13 +0800

    Complete day 7

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,6 +1,14 @@
 use std::collections::{HashMap, VecDeque};
 type Rule = (String, Vec<(String, u32)>);
 
+struct BagsGraph<'a> {
+    g: Vec<Vec<u32>>,
+    m: HashMap<&'a str, usize>
+}
+    
+
+
+
 pub fn solve(input: &str) {
     let rules = parse(input);
 
@@ -9,8 +17,8 @@ pub fn solve(input: &str) {
         .enumerate()
         .map(|(i, v)| (v.0.as_str(), i as usize))
         .collect();
-    let len = rules.len();
 
+    let len = rules.len();
     let mut g = vec![vec![0; len]; len];
 
     // construct graph
@@ -22,16 +30,44 @@ pub fn solve(input: &str) {
         }
     }
 
+    let graph = BagsGraph {
+        g: g,
+        m: bags_index
+    };
+
+    let init = graph.m.get("shiny gold").unwrap();
+
+    let num_of_colors = count_colors(&graph, *init);
+    println!("{}", num_of_colors);
+
+    let num_of_required_bags = count_total_required_bags(&graph, *init) - 1;
+    println!("{}", num_of_required_bags);
+}
+
+fn count_total_required_bags(g: &BagsGraph, color_index: usize) -> u64{
+    let mut count: u64 = 1; // including this bag
+
+    for (i, child) in g.g[color_index].iter().enumerate() {
+        if *child != 0 {
+            count += (*child as u64) * count_total_required_bags(g, i);
+        }
+    }
+
+    count
+}
+
+fn count_colors(g: &BagsGraph, init_index: usize) -> u32 {
+    let len = g.g.len();
+
     // bfs
     let mut q: VecDeque<usize> = VecDeque::new();
-    let init = bags_index.get("shiny gold").unwrap();
-    q.push_back(*init);
+    q.push_back(init_index);
     let mut visited = vec![false; len];
 
     while let Some(node) = q.pop_front() {
 
         for i in 0..len {
-            if g[i][node] != 0 && !visited[i] {
+            if g.g[i][node] != 0 && !visited[i] {
                 visited[i] = true;
                 q.push_back(i);
             }
@@ -39,8 +75,7 @@ pub fn solve(input: &str) {
     }
 
 
-    let bag_contains_shiny_gold = visited.iter().filter(|x| **x).count();
-    println!("{}", bag_contains_shiny_gold);
+    visited.iter().filter(|x| **x).count() as u32
 }
 
 fn parse(input: &str) -> Vec<Rule> {
