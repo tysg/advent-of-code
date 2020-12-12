2fc081a10413c9f00cc9a36ef6fa97010818db8e
Tianyi Song <42670338+tysng@users.noreply.github.com>
2020-12-12 19:23:11 +0800

    Complete day 12 part 2

--- src/day_12.rs
+++ src/day_12.rs
@@ -1,15 +1,17 @@
 #[derive(Debug, PartialEq)]
 struct Ship {
-    facing: u8, // North: 0; East: 1, South: 2, West: 3;
     x: i32,
     y: i32,
+    dx: i32,
+    dy: i32
 }
 
 pub fn solve(input: &str) {
     let start_state = Ship {
-        facing: 1,
         x: 0,
         y: 0,
+        dx: 10,
+        dy: 1
     };
     let result_state = input.lines().fold(start_state, |acc, i| next_state(acc, i));
 
@@ -20,205 +22,45 @@ pub fn solve(input: &str) {
 fn next_state(s: Ship, action: &str) -> Ship {
     let num = action[1..].parse::<i32>().unwrap();
     match &action[0..1] {
-        "N" => Ship { y: s.y + num, ..s },
-        "E" => Ship { x: s.x + num, ..s },
-        "S" => Ship { y: s.y - num, ..s },
-        "W" => Ship { x: s.x - num, ..s },
-        "L" => Ship {
-            facing: turn(s.facing, -num),
+        "N" => Ship { dy: s.dy + num, ..s },
+        "E" => Ship { dx: s.dx + num, ..s },
+        "S" => Ship { dy: s.dy - num, ..s },
+        "W" => Ship { dx: s.dx - num, ..s },
+        "L" => {
+            let (nx, ny) = turn((s.dx,s.dy), -num);
+            Ship {
+                dx: nx, dy:ny,
             ..s
-        },
-        "R" => Ship {
-            facing: turn(s.facing, num),
+        }},
+        "R" => {
+            let (nx, ny) = turn((s.dx,s.dy), num);
+            Ship {
+                dx: nx, dy:ny,
             ..s
+            }
         },
         "F" => Ship {
-            x: s.x + num * dx(s.facing),
-            y: s.y + num * dy(s.facing),
+            x: s.x + num * s.dx,
+            y: s.y + num * s.dy,
             ..s
         },
         _ => panic!("unexpected token"),
     }
 }
 
-fn turn(facing: u8, rotation: i32) -> u8 {
+fn turn(waypoint: (i32, i32), rotation: i32) -> (i32,i32){
     // assume all rotation % 90 == 0
     assert_eq!(rotation % 90, 0);
     let turns = rotation / 90;
-    ((facing as i32 + turns + 4) % 4) as u8
-}
+    let (x,y) = waypoint;
 
-fn dx(facing: u8) -> i32 {
-    match facing {
-        1 | 3 => 2 - facing as i32,
-        _ => 0,
-    }
-}
-fn dy(facing: u8) -> i32 {
-    match facing {
-        0 | 2 => 1 - facing as i32,
-        _ => 0,
+    match (turns + 4) % 4 {
+        0 => (x,y),
+        1 => (y,-x),
+        2 => (-x,-y),
+        3 => (-y,x),
+        _ => panic!("mathematically impossible")
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn turn_right() {
-        assert_eq!(turn(3, 180), 1);
-    }
-
-    #[test]
-    fn turn_left() {
-        assert_eq!(turn(0, -180), 2);
-    }
-
-    #[test]
-    fn next_state_fwd_n() {
-        assert_eq!(
-            next_state(
-                Ship {
-                    x: 0,
-                    y: 0,
-                    facing: 0
-                },
-                "F11"
-            ),
-            Ship {
-                x: 0,
-                y: 11,
-                facing: 0
-            }
-        );
-    }
-
-    #[test]
-    fn next_state_fwd_e() {
-        assert_eq!(
-            next_state(
-                Ship {
-                    x: 0,
-                    y: 0,
-                    facing: 1
-                },
-                "F11"
-            ),
-            Ship {
-                x: 11,
-                y: 0,
-                facing: 1
-            }
-        );
-    }
-    #[test]
-    fn next_state_fwd_s() {
-        assert_eq!(
-            next_state(
-                Ship {
-                    x: 0,
-                    y: 0,
-                    facing: 2
-                },
-                "F11"
-            ),
-            Ship {
-                x: 0,
-                y: -11,
-                facing: 2
-            }
-        );
-    }
-    #[test]
-    fn next_state_fwd_w() {
-        assert_eq!(
-            next_state(
-                Ship {
-                    x: 0,
-                    y: 0,
-                    facing: 3
-                },
-                "F11"
-            ),
-            Ship {
-                x: -11,
-                y: 0,
-                facing: 3
-            }
-        );
-    }
-    #[test]
-    fn next_state_n() {
-        assert_eq!(
-            next_state(
-                Ship {
-                    x: 0,
-                    y: 0,
-                    facing: 0
-                },
-                "N11"
-            ),
-            Ship {
-                x: 0,
-                y: 11,
-                facing: 0
-            }
-        );
-    }
-    #[test]
-    fn next_state_e() {
-        assert_eq!(
-            next_state(
-                Ship {
-                    x: 0,
-                    y: 0,
-                    facing: 0
-                },
-                "E11"
-            ),
-            Ship {
-                x: 11,
-                y: 0,
-                facing: 0
-            }
-        );
-    }
-    #[test]
-    fn next_state_s() {
-        assert_eq!(
-            next_state(
-                Ship {
-                    x: 0,
-                    y: 0,
-                    facing: 0
-                },
-                "S11"
-            ),
-            Ship {
-                x: 0,
-                y: -11,
-                facing: 0
-            }
-        );
-    }
-
-    #[test]
-    fn next_state_w() {
-        assert_eq!(
-            next_state(
-                Ship {
-                    x: 0,
-                    y: 0,
-                    facing: 0
-                },
-                "W11"
-            ),
-            Ship {
-                x: -11,
-                y: 0,
-                facing: 0
-            }
-        );
-    }
-}
