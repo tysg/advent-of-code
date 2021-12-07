6939a6c1b22464177473445570f74edd061c54e5
Tianyi Song <42670338+tysg@users.noreply.github.com>
2021-12-07 15:06:42 +0800

    Make it faster by type hints

--- 2021/src/advent_of_code/day-7.clj
+++ 2021/src/advent_of_code/day-7.clj
@@ -7,11 +7,11 @@
 (def input (slurp "resources/day7.in"))
 
 (defn parse-input [input] 
-  (map #(Integer/parseInt %) (string/split (string/trim input) #"," )))
+  (map #(Long/parseLong %) (string/split (string/trim input) #"," )))
 
-(defn euclidean-dist [a b] (Math/abs (- a b))) 
+(defn euclidean-dist [^long a ^long b] (Math/abs (- a b))) 
 
-(defn summation [n] (/ (* n (inc n)) 2)) 
+(defn summation [n] (quot (* n (inc n)) 2)) 
 
 (defn summation-dist [a b] (summation (euclidean-dist a b)))
 
@@ -21,8 +21,10 @@
 (defn solve 
   ([] (solve example))
   ([input] 
-   (let [numbers (parse-input input)]
-     (apply min (map #(fuel-use numbers % summation-dist) (range (apply min numbers) (inc (apply max numbers))))))))
+   (let [numbers (parse-input input)
+         nmin (apply min numbers)
+         nmax (apply max numbers)]
+     (apply min (map #(fuel-use numbers % summation-dist) (range nmin (inc nmax)))))))
 
 (comment
   (time (solve input)))
