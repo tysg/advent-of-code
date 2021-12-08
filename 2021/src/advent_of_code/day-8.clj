4cdeaebe408f5ad2230dc4622318b5d4fdb17b92
Tianyi Song <42670338+tysg@users.noreply.github.com>
2021-12-08 23:02:27 +0800

    Clean up project

--- 2021/src/advent_of_code/day-8.clj
+++ 2021/src/advent_of_code/day-8.clj
@@ -19,43 +19,37 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 
 (def input (slurp "resources/day8.in"))
 
-(defn parse-input [input]
-  (map  #(re-seq #"\w+" %) (string/split-lines (string/trim input))))
-
+(defn parse-line 
+  "return words, which is a seq of sets. each set contains the characters in a word"
+  [line] (->> line (re-seq #"\w+") (map #(into #{} %))))
 
 (defn build-word-map [words]
   (map #(vector (count %) %) words))
-
 
 (def mapping 
   "mapping from dash count to number"
   {3 7, 4 4, 2 1, 7 8})
 
+(defn part1 []
 
-(let [entries (parse-input input)
-      rhs-word-maps (map #(build-word-map (second %)) entries)]
-  
-  (reduce + 
-          (for [m rhs-word-maps                   
-                :let [freqs (map first m)]]
-            (count (filter mapping freqs))))
-  ) 
+  (let [entries (parse-input input)
+        rhs-word-maps (map #(build-word-map (second %)) entries)]
+    
+    (reduce + 
+            (for [m rhs-word-maps                   
+                  :let [freqs (map first m)]]
+              (count (filter mapping freqs)))))) 
 
 
 (def segments [\a \b \c \d \e \f \g])
 
-(def segment-perm (combo/permutations segments))
-
-(defn build-char-map [permutation] (zipmap segments permutation))
-
-(def segment-mapping 
+(def segments->digit
 {
  #{\a \b \c \e \f \g} \0
  #{\c \f} \1
  #{\a \c \d \e \g} \2
  #{\a \c \d \f \g} \3
  #{\b \c \d \f} \4
- 
  #{\a \b \d \f \g} \5
  #{\a \b \d \e \f \g} \6
  #{\a \c \f} \7
@@ -63,34 +57,33 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
  #{\a \b \c \d \f \g} \9
  })
 
-(defn replace-with-mapping [words mapping]
-  (for [w words] (replace mapping w)))
 
-(def all-mappings (map build-char-map segment-perm))
+(defn char-map [permutation] (zipmap segments permutation))
 
-(defn all-valid-digits? [words] 
-  (every? segment-mapping (map #(into #{} %) words)))
+(defn valid-digit? [word] (boolean (segments->digit word)))
 
+(defn replace-word [mapping word] (into #{} (replace mapping word)))
 
-(defn find-valid-mappings [words]
-  (filter
-   (fn [mapping] (all-valid-digits? (replace-with-mapping words mapping)))
-   all-mappings))
+(defn valid-mappings 
+  [words] 
+  (for [mapping (map char-map (combo/permutations segments))
+        :let [replaced (for [w words] (replace-word mapping w))]
+        :when (every? valid-digit? replaced)]
+    mapping))
 
+(defn output-value [words mapping]
+  (->> (for [w words] (segments->digit (replace-word mapping w)))
+       (take-last 4)
+       (string/join)
+       (Long/parseLong)))
 
-(let [lines (parse-input input)]
-  (reduce +  (map
-              (fn [words] 
-                (let [mapping (first (find-valid-mappings words))]
-                  (->> (replace-with-mapping words mapping)
-                       (map (fn [s] (into #{} s)))
-                       (map segment-mapping)
-                       (take-last 4)
-                       (string/join)
-                       (Long/parseLong)
-                       )))
-              lines))
-)
+(output-value '((\a \c \f)) {\a \a \c \c \f \f})
+
+(let [lines (->> input string/trim string/split-lines)
+      wordss (map parse-line lines)]
+  (reduce
+   +
+   (for [w wordss] (output-value w (first (valid-mappings w))))))
    
 
 
